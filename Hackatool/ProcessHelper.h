6a528af448d8205373f9fd969d4c2b1c85f388f8
unknown <juwon1@ualberta.ca>
2021-02-24 23:37:23 -0700

    Workinggit add ./! Can finally access and go through other process's memory pages

--- Hackatool/ProcessHelper.h
+++ Hackatool/ProcessHelper.h
@@ -4,12 +4,27 @@
 #include <vector>
 #include <string>
 
+struct MemoryPage {
+	char* baseAddress;
+	size_t size;
+
+	MemoryPage(char* address, size_t size) : baseAddress(address), size(size) {}
+};
+
 namespace ProcessHelper
 {
 	static void getAllRunningProcesses(std::vector<DWORD> &vec);
 	static std::string getProcessName(DWORD processID);
 	static DWORD findProcessID(std::string processName);
 	static void printAllProcesses();
+	static HANDLE getProcessHandle(DWORD processID);
+	static void closeProcess(HANDLE processHandle);
+
+	template <class T>
+	static void scanMemory(HANDLE handle, std::vector<MemoryPage> pageVec, T value);
+	static DWORD getBaseAddress(HANDLE processHandle);
+
+	static std::vector<MemoryPage> getMemoryPages(HANDLE processHandle);
 	//ProcessHelper() {  };
 	//static std::vector<DWORD> processVec;
 };
--- Hackatool/main.cpp
+++ Hackatool/main.cpp
@@ -3,11 +3,25 @@
 
 int main()
 {
-	ProcessHelper::printAllProcesses();
+	//ProcessHelper::printAllProcesses();
 	std::string str;
 	std::cin >> str;
-	DWORD processID = ProcessHelper::findProcessID(str);
+	DWORD processID;
+	//= ProcessHelper::findProcessID(str);
+	std::cin >> processID;
+	HANDLE handle = ProcessHelper::getProcessHandle(processID);
+	//int baseAddress = ProcessHelper::getBaseAddress(handle);
+	std::cout << "Process ID: " << processID << std::endl;
+	
+	std::vector<MemoryPage> pageVec = ProcessHelper::getMemoryPages(handle);
+	std::cout << "44 ID: " << processID << std::endl;
+	for (auto i : pageVec)
+	{
+		std::cout << i.baseAddress << std::endl;
+	}
+	std::cout << "Process ID: " << processID << std::endl;
 
+	ProcessHelper::scanMemory(handle, pageVec, 0.1f);
 	std::cout << "Process ID: " << processID << std::endl;
 	return 0;
 }
